d1896883d14f9415857d5fb743dcad341c1f35a1
Ozan ERTÜRK <ozanerturk17@gmail.com>
2020-11-28 20:48:10 +0300

    re-oriented telegramsend business

--- index.js
+++ index.js
@@ -68,17 +68,16 @@ async function update() {
             totalRecovered: data.toplam_iyilesen.replace(DOT_REGEX, ''),
             tests: data.gunluk_test.replace(DOT_REGEX, ''),
             cases: data.gunluk_vaka.replace(DOT_REGEX, ''),
-            patient:data.gunluk_hasta.replace(DOT_REGEX, ''),
+            patient: data.gunluk_hasta.replace(DOT_REGEX, ''),
             critical: data.agir_hasta_sayisi.replace(DOT_REGEX, ''),
             pneumoniaPercent: `%${data.hastalarda_zaturre_oran.replace(DOT_REGEX, ',')}`,
             deaths: data.gunluk_vefat.replace(DOT_REGEX, ''),
             recovered: data.gunluk_iyilesen.replace(DOT_REGEX, '')
         }
-        timeline[date] = dayData
-
-        if (Object.keys(timeline).indexOf(date) != -1) {
+        if (Object.keys(timeline).indexOf(date) == -1) {
             sendTelegram(dayData)
         }
+        timeline[date] = dayData
 
         console.log(dayData)
         const csv = new Parser(Object.keys(dayData)).parse(Object.values(timeline));
